75f3aa9656bac9627a6802cfade8dc151523d8b9
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-05-26 09:27:03 +0700

    tambah model peminjam

---
+++ 11317049_FedrickSiagian_uts/Models/Peminjam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _11317049_FedrickSiagian_uts.Models
+{
+    public class Peminjam
+    {
+        public string Nama { get; set; }
+        public string ID { get; set; }
+        public string Alamat { get; set; }
+        public string Phone { get; set; }
+    }
+}
