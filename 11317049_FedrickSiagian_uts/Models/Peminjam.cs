bb14dbda3746e7864725dd7f50ace1163c694af3
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-05-26 10:47:06 +0700

    Validasi Form

--- 11317049_FedrickSiagian_uts/Models/Peminjam.cs
+++ 11317049_FedrickSiagian_uts/Models/Peminjam.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace _11317049_FedrickSiagian_uts.Models
 {
     public class Peminjam
     {
+        [Required(ErrorMessage ="Mohon masukkan nama anda.")]
         public string Nama { get; set; }
+        [Required(ErrorMessage = "Mohon masukkan ID anda.")]
         public string ID { get; set; }
+        [Required(ErrorMessage = "Mohon masukkan E-mail anda.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage="Format e-mail salah")]
         public string Alamat { get; set; }
+        [Required(ErrorMessage = "Mohon masukkan Nomor Telepon anda.")]
+        [RegularExpression("^[0-9]{10,13}$", ErrorMessage = "Format Nomor Telepon salah, Mohon hanya gunakan angka.")]
         public string Phone { get; set; }
     }
 }
