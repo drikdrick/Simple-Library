2c53a8dbd833b5f40c618fb642469d6dd98757a5
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-05-26 11:37:19 +0700

    tambah data buku

--- 11317049_FedrickSiagian_uts/Controllers/HomeController.cs
+++ 11317049_FedrickSiagian_uts/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using _11317049_FedrickSiagian_uts.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace _11317049_FedrickSiagian_uts.Controllers
 {
@@ -10,32 +12,14 @@ namespace _11317049_FedrickSiagian_uts.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult FormPesan()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult FormPesan(Models.Peminjam Peminjam)
-        {
-            return View("Terimakasih", Peminjam);
+            Buku[] array =
+            {
+                new Buku{ kodeBuku="B101", judulBuku="Everything is Fucked", pengarangBuku="Mark Manson", tglTerbit="14 Mei 2019" },
+                new Buku{ kodeBuku="B102", judulBuku="C For Dummies", pengarangBuku="Dan Gookin", tglTerbit="14 Mei 2004" },
+                new Buku{ kodeBuku="B103", judulBuku="Teach C Yourself in 21 Days", pengarangBuku="Peter Aitken, Bradley L. Jones", tglTerbit="14 Mei 2000" },
+            };
+            return View(array);
         }
     }
+    
 }
---
+++ 11317049_FedrickSiagian_uts/Models/Buku.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _11317049_FedrickSiagian_uts.Models
+{
+    public class Buku
+    {
+        public string kodeBuku { get; set; }
+        public string judulBuku { get; set; }
+        public string pengarangBuku { get; set; }
+        public string tglTerbit { get; set; }
+    }
+}
